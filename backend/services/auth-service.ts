37a4a69ceb9f739bf0229d9edd35459f57037ae2
Tiffany <tiffanylarsson.stud@gmail.com>
2026-04-07 09:53:36 +0200

    Update auth-service.ts

--- backend/services/auth-service.ts
+++ backend/services/auth-service.ts
@@ -2,7 +2,7 @@
 import * as userModelModule from "../models/user-model.ts";
 import * as authModule from "../utils/auth.ts";
 
-export async function registerUserController(payload: any) {
+export async function registerUserService(payload: any) {
 
     const reqBody = payload.reqBody;
     const username = payload.username;
@@ -36,7 +36,7 @@ export async function registerUserController(payload: any) {
         password: hasedPassword,
         role: role || "user",
     });
-    
+
     // All OK -> create
     responseObj.status = 201;
     responseObj.body = {
@@ -46,3 +46,64 @@ export async function registerUserController(payload: any) {
     return responseObj;
 
 }
+
+export async function loginUserService(payload: any) {
+
+    const reqBody = payload.reqBody;
+    const email = payload.email;
+    let responseObj = {
+        status: 400,
+        body: {}
+    };
+
+    const foundUser = userModelModule.getUserByEmail(email);
+    if (!foundUser) {
+        responseObj.status = 401;
+        responseObj.body = { error: "Unvalid email or password." };
+        return responseObj;
+    }
+
+    const validPassword = await authModule.comparePassword(reqBody.password, foundUser.password);
+    if (!validPassword) {
+        responseObj.status = 401;
+        responseObj.body = { error: "Unvalid email or password." };
+        return responseObj;
+    }
+
+    const userPayload = {
+        id: foundUser.id,
+        email: foundUser.email,
+        role: foundUser.role,
+        exp: authModule.getTokenExpiration(60)
+    }
+    const jwt = await authModule.generateJwtToken(userPayload);
+
+    responseObj.status = 200;
+    responseObj.body = {
+        message: "Sign in successful!",
+        token: jwt,
+        user: {
+            id: foundUser.id,
+            email: foundUser.email,
+            role: foundUser.role,
+        }
+    };
+
+}
+
+export async function updateUserService(payload: any) {
+
+    const authHeader = payload.headers;
+    let responseObj = {
+        status: 400,
+        body: {}
+    };
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        responseObj.response.status = 401;
+        responseObj.response.body = { error: "Missing or invalid token" };
+        return;
+    }
+
+}
+
